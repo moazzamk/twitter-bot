61285a80118904ce281a13ceeb2f16d109cfba0b
moazzam khan <moazzam.khan@dialexa.com>
2020-07-19 12:06:52 -0500

    Dependencies and main.go emptied out

--- main.go
+++ main.go
@@ -1,25 +1,5 @@
-package twitter_bot
-
-import (
-	"fmt"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/clientcredentials"
-	"github.com/dghubble/go-twitter/twitter"
-)
+package main
 
 func main() {
-	config := &clientcredentials.Config{
-		ClientID: "consumerKey",
-		ClientSecret: "secret",
-		TokenURL: "https://api.twitter.com/oauth2/token",
-	}
 
-
-	httpClient := config.Client(oauth2.NoContext)
-
-	// Twitter client
-	client := twitter.NewClient(httpClient)
-
-	client
-	fmt.Println(client)
 }
